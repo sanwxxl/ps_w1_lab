7efc273ee334fc2cbe28977e77cf342b85222a33
sanwxxl <sanwool1755@gmail.com>
2026-03-09 21:52:02 +0900

    gitignore

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello GitHub2\n");
+    int size;
+    int stars, blank1, blank2;
+    int i, j;
+
+    printf("input size: ");
+    scanf("%d", &size);
+    blank1 = 0;
+    blank2 = size;
+    
+    for(i; i < size; i++){
+        printf(" " * "%d", blank1);
+    }
     return 0;
 }
